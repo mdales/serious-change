2e2b5113d0e3fbe006cfece069b5ec72f3c77018
Michael Dales <mdales@iver.local>
2008-10-12 12:33:30 +0100

    Made some changes based on live install and added a 404 page.

--- settings.py
+++ settings.py
@@ -35,7 +35,8 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/mdales/SeriousChange/website/seriouschange/media'
+MEDIA_ROOT = '/Users/mdales/SeriousChange/website/seriouschange/site_media'
+ADMIN_MEDIA_ROOT = '/Users/mdales/SeriousChange/website/seriouschange/site_media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from seriouschange import settings
 
 # Uncomment the next two lines to enable the admin:
 #from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('seriouschange.signup.views',
     # Example:
@@ -13,12 +13,14 @@ urlpatterns = patterns('seriouschange.signup.views',
     #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line for to enable the admin:
-    #(r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
 
 urlpatterns += patterns('django.views',
      (r'^site_media/(?P<path>.*)$', 'static.serve', 
          {'document_root': settings.MEDIA_ROOT}),
+     (r'^media/(?P<path>.*)$', 'static.serve', 
+         {'document_root': settings.ADMIN_MEDIA_ROOT}),
 
 )
 
